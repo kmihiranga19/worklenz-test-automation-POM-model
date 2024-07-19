fc1bb23f685050383d50e5b72a3d659f50b4b7f8
kavinduCeydigital <kmvithanage48@gmail.com>
2024-07-19 17:03:54 +0530

    invite_onboarding

--- page_objects/Invite/InviteOnboarding.py
+++ page_objects/Invite/InviteOnboarding.py
@@ -6,6 +6,8 @@ from page_objects.Locators import SignUpLocators, InviteOnboardingLocators
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from faker import Faker
+import time
+from test_cases.conftest import setup
 
 
 # class SignUp(Basepage):
@@ -34,30 +36,31 @@ from faker import Faker
 
 
 class InviteOnboarding(Basepage):
-    wait = WebDriverWait(driver, 10)
-    fake = Faker()
-    task_names = ["Planning", "Designing", "Coding", "Testing", "Maintaining", "Release"]
 
-    def __init__(self, browser):
-        super().__init__(browser)
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+        self.wait = WebDriverWait(self.driver, 10)
         self.locators = InviteOnboardingLocators()
+        self.fake = Faker()
+        self.task_names = ["Planning", "Designing", "Coding", "Testing", "Maintaining", "Release"]
 
     def signUp(self):
         random_name = self.fake.name()
         random_email = self.fake.email()
         self.wait.until(EC.visibility_of_element_located((By.ID, "full-name"))).send_keys(random_name)
-        wait.until(EC.visibility_of_element_located((By.ID, "email"))).send_keys(random_email)
-        wait.until(EC.visibility_of_element_located((By.ID, "password"))).send_keys("ceyDigital#00")
-        form = driver.find_element(By.TAG_NAME, "form")
+        self.wait.until(EC.visibility_of_element_located((By.ID, "email"))).send_keys(random_email)
+        self.wait.until(EC.visibility_of_element_located((By.ID, "password"))).send_keys("ceyDigital#00")
+        form = self.driver.find_element(By.TAG_NAME, "form")
         sign_up = form.find_elements(By.TAG_NAME, "button")[0]
         sign_up.click()
         time.sleep(2)
 
     def give_organization_name(self):
-        random_org_name = fake.name()
+        random_org_name = self.fake.name()
         organization = random_org_name + " Team"
-        wait.until(EC.visibility_of_element_located((By.TAG_NAME, "input"))).send_keys(organization)
-        continue_btn = driver.find_element(By.TAG_NAME, "button")
+        self.wait.until(EC.visibility_of_element_located((By.TAG_NAME, "input"))).send_keys(organization)
+        continue_btn = self.driver.find_element(By.TAG_NAME, "button")
         if continue_btn.is_enabled():
             continue_btn.click()
             time.sleep(2)
@@ -65,21 +68,21 @@ class InviteOnboarding(Basepage):
             print("Please enter valid organization name")
         return organization
 
-    def create_first_project():
-        project = fake.random_element(elements=('A', 'B', 'C', 'D'))
-        wait.until(EC.visibility_of_element_located((By.TAG_NAME, "input"))).send_keys(
+    def create_first_project(self):
+        project = self.fake.random_element(elements=('A', 'B', 'C', 'D'))
+        self.wait.until(EC.visibility_of_element_located((By.TAG_NAME, "input"))).send_keys(
             "First project" +
             project)
-        input_element = driver.find_element(By.TAG_NAME, "input")
+        input_element = self.driver.find_element(By.TAG_NAME, "input")
         first_project_name = input_element.get_attribute("value")
-        continue_btn = driver.find_element(By.XPATH, "//span[normalize-space()='Continue']")
+        continue_btn = self.driver.find_element(By.XPATH, "//span[normalize-space()='Continue']")
         continue_btn.click()
         time.sleep(5)
         return first_project_name
 
-    def create_first_tasks():
-        add_another_task = driver.find_element(By.XPATH, "//span[normalize-space()='Add another']")
-        continue_btn = driver.find_element(By.XPATH, "//span[normalize-space()='Continue']")
+    def create_first_tasks(self):
+        add_another_task = self.driver.find_element(By.XPATH, "//span[normalize-space()='Add another']")
+        continue_btn = self.driver.find_element(By.XPATH, "//span[normalize-space()='Continue']")
 
         i = 0
 
@@ -90,15 +93,15 @@ class InviteOnboarding(Basepage):
         time.sleep(3)
 
         for i in range(6):
-            element = driver.find_element(By.ID, f"task-name-input-{i}")
-            element.send_keys(task_names[i])
+            element = self.driver.find_element(By.ID, f"task-name-input-{i}")
+            element.send_keys(self.task_names[i])
 
         time.sleep(2)
         continue_btn.click()
 
-    def invite_team_members():
-        add_another_task = driver.find_element(By.XPATH, "//span[normalize-space()='Add another']")
-        continue_btn = driver.find_element(By.XPATH, "//span[normalize-space()='Continue']")
+    def invite_team_members(self):
+        add_another_task = self.driver.find_element(By.XPATH, "//span[normalize-space()='Add another']")
+        continue_btn = self.driver.find_element(By.XPATH, "//span[normalize-space()='Continue']")
 
         i = 0
 
@@ -106,11 +109,11 @@ class InviteOnboarding(Basepage):
             add_another_task.click()
             i += 1
 
-        menu = driver.find_element(By.TAG_NAME, "nz-list")
+        menu = self.driver.find_element(By.TAG_NAME, "nz-list")
         inputs = menu.find_elements(By.TAG_NAME, "input")
 
         for i in range(3):
-            random_email = fake.email()
+            random_email = self.fake.email()
             inputs[i].send_keys(random_email)
 
         if continue_btn.is_enabled():
@@ -122,55 +125,55 @@ class InviteOnboarding(Basepage):
 
         time.sleep(10)
 
-    def skip_invite_team_members():
-        skip_btn = driver.find_element(By.CSS_SELECTOR, "span[class='ant-typography']")
+    def skip_invite_team_members(self):
+        skip_btn = self.driver.find_element(By.CSS_SELECTOR, "span[class='ant-typography']")
         skip_btn.click()
 
-    def get_organization_name():
-        header = driver.find_element(By.TAG_NAME, "worklenz-header")
+    def get_organization_name(self):
+        header = self.driver.find_element(By.TAG_NAME, "worklenz-header")
         header_div = header.find_elements(By.TAG_NAME, "div")[1]
         header_ul = header_div.find_elements(By.TAG_NAME, "ul")[1]
         team_selection = header_ul.find_elements(By.TAG_NAME, "li")[0]
         team_selection.click()
         time.sleep(1)
-        team_selection_items = driver.find_element(By.CLASS_NAME, "align-items-baseline")
+        team_selection_items = self.driver.find_element(By.CLASS_NAME, "align-items-baseline")
         team = team_selection_items.find_elements(By.TAG_NAME, "span")[0]
         organization_text = team.text.strip()
         return organization_text
 
-    def get_project_name():
-        page_header_title = driver.find_element(By.TAG_NAME, "nz-page-header-title")
+    def get_project_name(self):
+        page_header_title = self.driver.find_element(By.TAG_NAME, "nz-page-header-title")
         project_title = page_header_title.find_element(By.CLASS_NAME, "project-title")
         project_text = project_title.text.strip()
         return project_text
 
-    def get_project_tasks():
+    def get_project_tasks(self):
         tasks = []
-        task_rows = driver.find_elements(By.TAG_NAME, "worklenz-task-list-row")
+        task_rows = self.driver.find_elements(By.TAG_NAME, "worklenz-task-list-row")
         for task_row in task_rows:
             task_name = task_row.find_element(By.CLASS_NAME, "task-name-text")
             tasks.append(task_name.text.strip())
 
         return tasks
 
-    def check_organization_name(org_name, created_org_name):
+    def check_organization_name(self, org_name, created_org_name):
         if created_org_name == org_name:
             print("Organization successfully created")
 
         else:
             print("Your entered organization not created")
 
-    def check_project_name(first_project_name, created_first_project_name):
+    def check_project_name(self, first_project_name, created_first_project_name):
         if created_first_project_name == first_project_name:
             print("First project successfully created")
 
         else:
             print("Your entered project not created")
 
-    def check_project_tasks(created_tasks):
-        if len(task_names) == len(created_tasks):
+    def check_project_tasks(self, created_tasks):
+        if len(self.task_names) == len(created_tasks):
             for created_task in created_tasks:
-                if created_task in task_names:
+                if created_task in self.task_names:
                     print(created_task + " task successfully created")
 
                 else:
--- test_cases/invite/test_invite_onbording.py
+++ test_cases/invite/test_invite_onbording.py
@@ -1,9 +1,8 @@
-from page_objects.Invite.InviteOnboarding import SignUp
 from page_objects.Invite.InviteOnboarding import InviteOnboarding
 import time
 from test_data.test_data import InviteData
 from utilities.readProperties import ReadConfig
-from self import self
+from selenium.webdriver.remote.webdriver import WebDriver
 
 
 class TestInviteOnboarding:
@@ -12,35 +11,52 @@ class TestInviteOnboarding:
     email = ReadConfig.get_inviteEmail()
     password = ReadConfig.get_password()
 
-    def test_invite_onboarding(self, setup):
-        self.browser = setup
-        self.browser.get(self.baseURL)
-        self.Invite = InviteData()
-        self.su = SignUp(self.browser)
-        time.sleep(3)
-        self.su.clickSignBtn()
-        time.sleep(3)
-        self.su.enterName(self.username)
-        self.su.enterEmail(self.email)
-        self.su.enterPassword(self.password)
-        time.sleep(3)
-        self.su.clickSubmitBtn()
-        time.sleep(3)
-        self.it = InviteOnboarding(self.browser)
-        time.sleep(5)
-        self.it.enterOrganizationName(self.Invite.orgName)
-        time.sleep(3)
-        self.it.clickContinueBtn1()
-        time.sleep(3)
-        self.it.enterProjectName(self.Invite.project)
-        time.sleep(3)
-        self.it.clickContinueBtn1()
-        time.sleep(3)
-        self.it.enterTask(self.Invite.task)
-        time.sleep(3)
-        self.it.clickContinueBtn1()
-        time.sleep(3)
-        self.it.enterInviteEmail(self.Invite.inviteEmail)
-        time.sleep(3)
-        self.it.clickContinueBtn1()
-        time.sleep(15)
+    def test_project_Invite(self, setup):
+        self.driver = setup
+        self.driver.get("https://uat.app.worklenz.com/auth/signup")
+        self.lg = InviteOnboarding(self.driver)
+        self.lg.signUp()
+        self.organization_name = self.lg.give_organization_name()
+        self.lg.create_first_project()
+        self.project_name = self.lg.create_first_tasks()
+        self.lg.invite_team_members()
+        self.lg.skip_invite_team_members()
+        self.created_organization_name = self.lg.get_organization_name()
+        self.created_project_name = self.lg.get_project_name()
+        self.created_tasks_name = self.lg.get_project_tasks()
+        self.lg.check_organization_name(self.organization_name, self.created_organization_name)
+        self.lg.check_project_name(self.project_name, self.created_project_name)
+        self.lg.check_project_tasks(self.created_tasks_name)
+
+
+
+
+        # self.Invite = InviteData()
+        # self.su = SignUp(self.browser)
+        # time.sleep(3)
+        # self.su.clickSignBtn()
+        # time.sleep(3)
+        # self.su.enterName(self.username)
+        # self.su.enterEmail(self.email)
+        # self.su.enterPassword(self.password)
+        # time.sleep(3)
+        # self.su.clickSubmitBtn()
+        # time.sleep(3)
+        # self.it = InviteOnboarding(self.browser)
+        # time.sleep(5)
+        # self.it.enterOrganizationName(self.Invite.orgName)
+        # time.sleep(3)
+        # self.it.clickContinueBtn1()
+        # time.sleep(3)
+        # self.it.enterProjectName(self.Invite.project)
+        # time.sleep(3)
+        # self.it.clickContinueBtn1()
+        # time.sleep(3)
+        # self.it.enterTask(self.Invite.task)
+        # time.sleep(3)
+        # self.it.clickContinueBtn1()
+        # time.sleep(3)
+        # self.it.enterInviteEmail(self.Invite.inviteEmail)
+        # time.sleep(3)
+        # self.it.clickContinueBtn1()
+        # time.sleep(15)
